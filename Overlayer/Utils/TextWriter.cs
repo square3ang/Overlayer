0284f4d688d57419b9e21851c5a4fc4468fb5b1f
C#Newbie <dongsu45681111@gmail.com>
2024-09-13 22:50:46 +0900

    add textwriter

---
+++ Overlayer/Utils/TextWriter.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.IO;
+
+namespace Overlayer.Utils
+{
+    public class TextWriter
+    {
+        private static readonly Font defFont = new Font(FontFamily.GenericSerif, 8);
+        private Font font = defFont;
+
+        public Color Color = Color.White;
+        public StringFormat Format = new StringFormat()
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center,
+        };
+
+        public bool TrySetFont(string fileName, int emSize)
+        {
+            if (!File.Exists(fileName)) return false;
+            using (var fonts = new PrivateFontCollection())
+            {
+                fonts.AddFontFile(fileName);
+                font = new Font(fonts.Families[0], emSize);
+            }
+            return true;
+        }
+        public void Write(Image target, PointF pt, string text)
+        {
+            RectangleF rectf = new RectangleF(pt.X, pt.Y, target.Width - pt.X, target.Height - pt.Y);
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                g.DrawString(text, font, new SolidBrush(Color), rectf, Format);
+                g.Flush();
+            }
+        }
+        public void Write(Image target, float x, float y, string text) => Write(target, new PointF(x, y), text);
+    }
+}
