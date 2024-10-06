74cd5647ca17143026ccd8f63e8a2dee4b2bb2bc
square3ang <square3ang@gmail.com>
2024-10-06 21:09:38 +0900

    fix 3

--- Overlayer/Patches/BlockUMMClosing.cs
+++ Overlayer/Patches/BlockUMMClosing.cs
@@ -6,11 +6,12 @@ public static class BlockUMMClosing
 {
     public static bool Block;
 
-    [LazyPatch("Patches.BlockUMMClosing", "UnityModManagerNet.UnityModManager.UI", "ToggleWindow", new string[] { "System.Boolean" })]
+    [LazyPatch("Patches.BlockUMMClosing", "UnityModManagerNet.UnityModManager+UI", "ToggleWindow", new string[] { "System.Boolean" })]
     public static class BlockUMMClosingPatch
     {
         public static bool Prefix()
         {
+            
             return !Block;
         }
     }
