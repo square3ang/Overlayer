30ccff490154b7f151ed0762e0b4e3e436b914b7
Kkitut <digitalhdmiport@gmail.com>
2024-10-10 00:20:21 +0900

    Add: UndoRedoManager

---
+++ Overlayer/Core/UndoRedoManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Overlayer.Core
+{
+    public class UndoRedoManager
+    {
+        private Stack<string> undoStack = new Stack<string>();
+        private Stack<string> redoStack = new Stack<string>();
+
+        public void SaveState(string state)
+        {
+            undoStack.Push(state);
+            redoStack.Clear();
+        }
+
+        public string Undo()
+        {
+            if(undoStack.Count > 0)
+            {
+                string state = undoStack.Pop();
+                redoStack.Push(state);
+                return undoStack.Count > 0 ? undoStack.Peek() : null;
+            }
+            return null;
+        }
+
+        public string Redo()
+        {
+            if(redoStack.Count > 0)
+            {
+                string state = redoStack.Pop();
+                undoStack.Push(state);
+                return state;
+            }
+            return null;
+        }
+    }
+}
