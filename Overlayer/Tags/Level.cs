87b94cd18a8cfd16370cb573bd334cf928cf58a0
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2025-08-05 22:36:20 +0900

    Fix an issue where the Artist and Author Tag remained the same when switching from scnGame to scene based maps

--- Overlayer/Tags/Level.cs
+++ Overlayer/Tags/Level.cs
@@ -8,9 +8,21 @@ namespace Overlayer.Tags
         [Tag]
         public static string Title(int maxLength = -1, string afterTrimStr = Extensions.DefaultTrimStr) => ADOBase.isOfficialLevel ? ADOBase.sceneName.Trim(maxLength, afterTrimStr) : ADOFAI.LevelData?.song?.BreakRichTag()?.Trim(maxLength, afterTrimStr);
         [Tag]
-        public static string Author(int maxLength = -1, string afterTrimStr = Extensions.DefaultTrimStr) => ADOFAI.LevelData?.author?.BreakRichTag()?.Trim(maxLength, afterTrimStr);
+        public static string Author(int maxLength = -1, string afterTrimStr = Extensions.DefaultTrimStr) {
+            if(scnGame.instance != null) {
+                return ADOFAI.LevelData?.author?.BreakRichTag()?.Trim(maxLength, afterTrimStr);
+            } else {
+                return string.Empty;
+            }
+        }
         [Tag]
-        public static string Artist(int maxLength = -1, string afterTrimStr = Extensions.DefaultTrimStr) => ADOFAI.LevelData?.artist?.BreakRichTag()?.Trim(maxLength, afterTrimStr);
+        public static string Artist(int maxLength = -1, string afterTrimStr = Extensions.DefaultTrimStr) {
+            if(scnGame.instance != null) {
+                return ADOFAI.LevelData?.artist.BreakRichTag()?.Trim(maxLength, afterTrimStr);
+            } else {
+                return string.Empty;
+            }
+        }
         [Tag]
         public static string TitleRaw(int maxLength = -1, string afterTrimStr = Extensions.DefaultTrimStr) => ADOBase.isOfficialLevel ? ADOBase.sceneName.Trim(maxLength, afterTrimStr) : ADOFAI.LevelData?.song?.Trim(maxLength, afterTrimStr);
         [Tag]
