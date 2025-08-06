cefff3095b14ea520adc22e4c6d721a83dc48888
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2025-08-06 20:57:33 +0900

    Delete TitleButton, Edit DrawAlignment

--- Overlayer/Core/Drawer.cs
+++ Overlayer/Core/Drawer.cs
@@ -156,17 +156,6 @@ namespace Overlayer.Core
             return c;
         }
 
-        public static void TitleButton(string label, string btnLabel, Action pressed, Action horizontal = null)
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label(label);
-            if (Drawer.Button(btnLabel))
-                pressed?.Invoke();
-            horizontal?.Invoke();
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-        }
-
         public static bool DrawSingleWithSlider(string label, ref float value, float lValue, float rValue, float width)
         {
             GUILayout.BeginHorizontal();
@@ -623,10 +612,8 @@ namespace Overlayer.Core
         }
 
         public static bool DrawAlignment(ref TextAlignmentOptions value) {
-            Color active = new Color(0f, 1f, 1f);
-
             if(value == TextAlignmentOptions.Converted) {
-                GUI.color = active;
+                GUI.color = Color.cyan;
                 ButtonImage(ali_Unknown, GUILayout.Width(404));
                 GUI.color = Color.white;
                 return false;
@@ -638,42 +625,42 @@ namespace Overlayer.Core
             GUILayout.BeginHorizontal();
 
             // Left 0
-            GUI.color = (((int)value & (1 << 0)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 0)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Left, GUILayout.Width(40))) {
                 newvalue &= ~0xFF;         // clear 0~7
                 newvalue |= (1 << 0);
             }
 
             // Center 1
-            GUI.color = (((int)value & (1 << 1)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 1)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Center, GUILayout.Width(40))) {
                 newvalue &= ~0xFF;
                 newvalue |= (1 << 1);
             }
 
             // Right 2
-            GUI.color = (((int)value & (1 << 2)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 2)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Right, GUILayout.Width(40))) {
                 newvalue &= ~0xFF;
                 newvalue |= (1 << 2);
             }
 
             // Justified 3
-            GUI.color = (((int)value & (1 << 3)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 3)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Justified, GUILayout.Width(40))) {
                 newvalue &= ~0xFF;
                 newvalue |= (1 << 3);
             }
 
             // Flush 4
-            GUI.color = (((int)value & (1 << 4)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 4)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Flush, GUILayout.Width(40))) {
                 newvalue &= ~0xFF;
                 newvalue |= (1 << 4);
             }
 
             // Geometry_Center 5
-            GUI.color = (((int)value & (1 << 5)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 5)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Geometry_Center, GUILayout.Width(40))) {
                 newvalue &= ~0xFF;
                 newvalue |= (1 << 5);
@@ -682,42 +669,42 @@ namespace Overlayer.Core
             GUILayout.Space(20);
 
             // Top 8
-            GUI.color = (((int)value & (1 << 8)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 8)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Top, GUILayout.Width(40))) {
                 newvalue &= ~(0xFF << 8);  // clear 8~15
                 newvalue |= (1 << 8);
             }
 
             // Middle 9
-            GUI.color = (((int)value & (1 << 9)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 9)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Middle, GUILayout.Width(40))) {
                 newvalue &= ~(0xFF << 8);
                 newvalue |= (1 << 9);
             }
 
             // Bottom 10
-            GUI.color = (((int)value & (1 << 10)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 10)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Bottom, GUILayout.Width(40))) {
                 newvalue &= ~(0xFF << 8);
                 newvalue |= (1 << 10);
             }
 
             // Baseline 11
-            GUI.color = (((int)value & (1 << 11)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 11)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Baseline, GUILayout.Width(40))) {
                 newvalue &= ~(0xFF << 8);
                 newvalue |= (1 << 11);
             }
 
             // Midline 12
-            GUI.color = (((int)value & (1 << 12)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 12)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Midline, GUILayout.Width(40))) {
                 newvalue &= ~(0xFF << 8);
                 newvalue |= (1 << 12);
             }
 
             // Capline 13
-            GUI.color = (((int)value & (1 << 13)) != 0) ? active : Color.white;
+            GUI.color = (((int)value & (1 << 13)) != 0) ? Color.cyan : Color.white;
             if(ButtonImage(ali_Capline, GUILayout.Width(40))) {
                 newvalue &= ~(0xFF << 8);
                 newvalue |= (1 << 13);
