668787b3f42836d0dba6bf122fec225436a522ca
Kkitut <digitalhdmiport@gmail.com>
2025-10-21 07:19:53 +0900

    Dll safe & optimize

--- Overlayer.Scripting.Bootstrapper/Main.cs
+++ Overlayer.Scripting.Bootstrapper/Main.cs
@@ -1,23 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using static UnityModManagerNet.UnityModManager;
 
-namespace Overlayer.Scripting.Bootstrapper
-{
-    public static class Main
-    {
-        public static void Load(ModEntry modEntry)
-        {
+namespace Overlayer.Scripting.Bootstrapper {
+    public static class Main {
+        private static readonly string[] requiredDlls = new[] { "JSNet", "Vostok" };
+        public static void Load(ModEntry modEntry) {
             var domain = AppDomain.CurrentDomain;
-            var jsNet = File.ReadAllBytes(Path.Combine(modEntry.Path, "JSNet.dll"));
-            var vostok = File.ReadAllBytes(Path.Combine(modEntry.Path, "Vostok.dll"));
-            var scripting = File.ReadAllBytes(Path.Combine(modEntry.Path, "Overlayer.Scripting.dll"));
-            domain.Load(jsNet);
-            domain.Load(vostok);
-            var scriptingAss = domain.Load(scripting);
-            typeof(ModEntry).GetField("mAssembly", (BindingFlags)15420).SetValue(modEntry, scriptingAss);
-            scriptingAss.GetType("Overlayer.Scripting.Main").GetMethod("Load").Invoke(null, new object[] { modEntry });
+
+            string libPath = Path.Combine(modEntry.Path, "lib");
+            if(!Directory.Exists(libPath)) {
+                modEntry.Logger.Log("/lib/ folder does not exist.");
+                return;
+            }
+
+            var dllFiles = Directory.GetFiles(libPath, "*.dll", SearchOption.AllDirectories);
+            modEntry.Logger.Log($"Found {dllFiles.Length} DLL(s) in /lib/");
+
+            var loadedTitles = new HashSet<string>();
+
+            foreach(var dllPath in dllFiles) {
+                try {
+                    var bytes = File.ReadAllBytes(dllPath);
+                    var assembly = Assembly.Load(bytes);
+                    modEntry.Logger.Log($"Loaded assembly {assembly.FullName} from {dllPath}");
+
+                    var productAttr = assembly.GetCustomAttribute<AssemblyProductAttribute>();
+                    if(productAttr != null) {
+                        loadedTitles.Add(productAttr.Product);
+                        modEntry.Logger.Log($"AssemblyTitle: {productAttr.Product}");
+                    }
+                } catch(Exception e) {
+                    modEntry.Logger.Log($"Failed to load {dllPath}: {e}");
+                }
+            }
+
+            foreach(var required in requiredDlls) {
+                if(!loadedTitles.Contains(required)) {
+                    modEntry.Logger.Log($"[ERROR] Required assembly with title '{required}' not loaded. Aborting scripting load.");
+                    return;
+                }
+            }
+
+            string scriptingPath = Path.Combine(modEntry.Path, "Overlayer.Scripting.dll");
+            if(!File.Exists(scriptingPath)) {
+                modEntry.Logger.Log("Overlayer.Scripting.dll not found");
+                return;
+            }
+
+            try {
+                var scriptingAss = Assembly.Load(File.ReadAllBytes(scriptingPath));
+                modEntry.Logger.Log("Loaded Overlayer.Scripting.dll successfully");
+
+                typeof(ModEntry)
+                    .GetField("mAssembly", (BindingFlags)15420)
+                    .SetValue(modEntry, scriptingAss);
+
+                scriptingAss
+                    .GetType("Overlayer.Scripting.Main")
+                    .GetMethod("Load")
+                    .Invoke(null, new object[] { modEntry });
+
+                modEntry.Logger.Log("Overlayer.Scripting.Main.Load invoked successfully");
+            } catch(Exception e) {
+                modEntry.Logger.Log($"Failed to load or invoke Overlayer.Scripting.dll: {e}");
+            }
         }
     }
 }
