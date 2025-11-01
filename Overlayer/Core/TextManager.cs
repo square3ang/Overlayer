0fd01be4b2aa2768a76756180931862f7f1e5a32
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2025-11-01 14:47:09 +0900

    Add MoveTextToIndex

--- Overlayer/Core/TextManager.cs
+++ Overlayer/Core/TextManager.cs
@@ -63,63 +63,31 @@ namespace Overlayer.Core
             return Texts[index];
         }
         public static OverlayerText Find(TextConfig configRef) => Texts.Find(ot => ReferenceEquals(ot.Config, configRef));
-        public static void MoveTextUp(int index) {
-            if(index <= 0 || index >= Texts.Count) {
-                return;
-            }
-            var tmp = Texts[index - 1];
-            Texts[index - 1] = Texts[index];
-            Texts[index] = tmp;
 
-            var go = Texts[index].gameObject;
-            var goPrev = Texts[index - 1].gameObject;
-            int prevSibling = goPrev.transform.GetSiblingIndex();
-            goPrev.transform.SetSiblingIndex(go.transform.GetSiblingIndex());
-            go.transform.SetSiblingIndex(prevSibling);
+        public static bool MoveTextToIndex(int from, int to) {
+            if(from < 0 || from >= Texts.Count || to < 0 || to >= Texts.Count || from == to)
+                return false;
 
-            Refresh();
+            var item = Texts[from];
+            Texts.RemoveAt(from);
+            Texts.Insert(to, item);
+
+            item.gameObject.transform.SetSiblingIndex(to);
+            return true;
         }
-        public static void MoveTextDown(int index) {
-            if(index < 0 || index >= Texts.Count - 1) {
-                return;
-            }
-            var tmp = Texts[index + 1];
-            Texts[index + 1] = Texts[index];
-            Texts[index] = tmp;
-
-            var go = Texts[index].gameObject;
-            var goNext = Texts[index + 1].gameObject;
-            int nextSibling = goNext.transform.GetSiblingIndex();
-            goNext.transform.SetSiblingIndex(go.transform.GetSiblingIndex());
-            go.transform.SetSiblingIndex(nextSibling);
-
-            Refresh();
+        public static bool MoveTextUp(int index) {
+            return MoveTextToIndex(index, index - 1);
         }
-        public static void MoveTextToTop(int index) {
-            if(index <= 0 || index >= Texts.Count) {
-                return;
-            }
-            var item = Texts[index];
-            Texts.RemoveAt(index);
-            Texts.Insert(0, item);
-
-            item.gameObject.transform.SetSiblingIndex(0);
-
-            Refresh();
+        public static bool MoveTextDown(int index) {
+            return MoveTextToIndex(index, index + 1);
         }
-        public static void MoveTextToBottom(int index) {
-            if(index < 0 || index >= Texts.Count - 1) {
-                return;
-            }
-
-            var item = Texts[index];
-            Texts.RemoveAt(index);
-            Texts.Add(item);
-
-            item.gameObject.transform.SetSiblingIndex(item.gameObject.transform.parent.childCount - 1);
-
-            Refresh();
+        public static bool MoveTextToTop(int index) {
+            return MoveTextToIndex(index, 0);
         }
+        public static bool MoveTextToBottom(int index) {
+            return MoveTextToIndex(index, Texts.Count - 1);
+        }
+
         public static void Remove(int index) => DestroyText(Texts[index]);
         public static void DestroyText(OverlayerText text)
         {
