a45166fa621b656851a82bc9bb5b049901d40ea9
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2025-11-07 19:30:24 +0900

    3.38.0

--- Overlayer/Properties/AssemblyInfo.cs
+++ Overlayer/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("a10da92a-c5bb-4708-b1b4-9a112a644d8c")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.37.1.0")]
+[assembly: AssemblyFileVersion("3.38.0.0")]
 [assembly: InternalsVisibleTo("Overlayer.Scripting")]
 [assembly: NeutralResourcesLanguage("")]
