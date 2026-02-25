e156dda38524a87516f2d312cca1c40977ae2e59
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-02-26 04:24:08 +0900

    3.41.1

--- Overlayer/Properties/AssemblyInfo.cs
+++ Overlayer/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("a10da92a-c5bb-4708-b1b4-9a112a644d8c")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.41.0.0")]
+[assembly: AssemblyFileVersion("3.41.1.0")]
 
 [assembly: NeutralResourcesLanguage("")]
 
