1afaa268bdaa2aad05b4cbc3686cd5b8d78bde44
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-03-17 02:28:35 +0900

    sprite!!

--- Overlayer/Unity/OverlayerImage.cs
+++ Overlayer/Unity/OverlayerImage.cs
@@ -105,7 +105,7 @@ public class OverlayerImage : OverlayerObject, IPointerDownHandler, IPointerUpHa
 
     public void ApplyImages() {
         Images.Clear();
-        _mainImage = null;
+        _mainImage.sprite = null;
         for(int i = 0; i < _config.Images.Count; i++) {
             string imagePath = _config.Images[i];
             if(imagePath != null) {
