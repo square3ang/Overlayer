765cf6d0e7c2ce40d73e90b4849817d4d23e2326
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-04-08 01:55:02 +0900

    Use Modified Jint

--- Overlayer/Core/Scripting/JSNet/API/Api.cs
+++ Overlayer/Core/Scripting/JSNet/API/Api.cs
@@ -1,13 +1,11 @@
 using Jint;
-using Jint.Native;
-using Jint.Runtime;
 using Jint.Runtime.Interop.Attributes;
+using Overlayer.Core.Scripting.JSNet.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using UnityEngine;
 
 namespace Overlayer.Core.Scripting.JSNet.API;
 
@@ -115,76 +113,17 @@ public class Api {
     }
 
     public Engine PrepareInterpreter() {
-        var engine = new Engine(op => {
-            op.AllowClr(AppDomain.CurrentDomain.GetAssemblies());
-            op.Strict(false);
+        Engine engine = new(op => {
+            op.AllowClrWrite().AllowReflection().ClrSlowInvoke(invoke: false)
+                .Strict(strict: false);
         });
-
         foreach(var (apiAttribute, type) in Types) {
-            var name = apiAttribute.Name ?? type.Name;
-
-            engine.SetValue(name, new TypeWrapper(type));
+            engine.SetValue(apiAttribute.Name ?? type.Name, type);
         }
-
-        foreach(var (apiAttribute, methodInfo) in Methods) {
-            var name = apiAttribute.Name ?? methodInfo.Name;
-
-            engine.SetValue(name, CreateFunction(methodInfo, engine));
+        foreach(var (apiAttribute2, methodInfo) in Methods) {
+            engine.SetValue(apiAttribute2.Name ?? methodInfo.Name, methodInfo);
         }
-
         return engine;
-    }
-
-    private static Delegate CreateFunction(MethodInfo method, Engine engine) {
-        return new Func<JsValue, JsValue[], JsValue>((thisObj, jsArgs) => {
-            try {
-                var parameters = method.GetParameters();
-                int pCount = parameters.Length;
-                var finalArgs = new object[pCount];
-                jsArgs ??= [];
-
-                bool hasParams = pCount > 0 && parameters[pCount - 1].GetCustomAttribute<ParamArrayAttribute>() != null;
-                int normalCount = hasParams ? pCount - 1 : pCount;
-
-                for(int i = 0; i < normalCount; i++) {
-                    var p = parameters[i];
-                    if(p.ParameterType == typeof(Engine)) {
-                        finalArgs[i] = engine;
-                        continue;
-                    }
-
-                    if(i < jsArgs.Length) {
-                        finalArgs[i] = engine.TypeConverter.Convert(jsArgs[i], p.ParameterType, null);
-                    } else if(p.HasDefaultValue) {
-                        finalArgs[i] = p.DefaultValue;
-                    } else {
-                        finalArgs[i] = p.ParameterType.IsValueType ? Activator.CreateInstance(p.ParameterType) : null;
-                    }
-                }
-
-                if(hasParams) {
-                    var lastParam = parameters[pCount - 1];
-                    var elementType = lastParam.ParameterType.GetElementType();
-                    int remainingJsArgs = Math.Max(0, jsArgs.Length - normalCount);
-
-                    var paramArray = Array.CreateInstance(elementType, remainingJsArgs);
-                    for(int i = 0; i < remainingJsArgs; i++) {
-                        var val = engine.TypeConverter.Convert(jsArgs[normalCount + i], elementType, null);
-                        paramArray.SetValue(val, i);
-                    }
-
-                    finalArgs[pCount - 1] = paramArray;
-                }
-
-                var result = method.Invoke(null, finalArgs);
-                return result == null ? JsValue.Null : JsValue.FromObject(engine, result);
-
-            } catch(TargetInvocationException tie) {
-                throw tie.InnerException ?? tie;
-            } catch(Exception e) {
-                throw new JavaScriptException(engine.Intrinsics.Error, e.Message);
-            }
-        });
     }
 
     public static ApiAttribute Get(Type t, Api api = null) => api?.Types.Find(((ApiAttribute, Type) tup) => tup.Item2 == t).Item1 ?? t.GetCustomAttribute<ApiAttribute>();
