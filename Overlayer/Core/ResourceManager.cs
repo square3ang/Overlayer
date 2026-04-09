8c3cca45d9a9960d8d1c22776727e3c3dcf5ae1a
Kkitut <digitalhdmiport@gmail.com>
2026-04-09 13:11:29 +0900

    Change some Resource

---
+++ Overlayer/Core/ResourceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Overlayer.Core;
+
+public static class ResourceManager
+{
+    public static byte[] GetResourceBytes(string resourceName) {
+        Assembly assembly = typeof(ResourceManager).Assembly;
+        string fullName = $"Overlayer.MiscFiles.res.{resourceName}";
+
+        using Stream stream = assembly.GetManifestResourceStream(fullName);
+        if(stream == null) {
+            return null;
+        }
+
+        using MemoryStream ms = new();
+        stream.CopyTo(ms);
+        return ms.ToArray();
+    }
+
+    public static byte[] GetImageBytes(string path) {
+        return GetResourceBytes($"images.{path}");
+    }
+
+}
