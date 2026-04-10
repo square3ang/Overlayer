4e72745858aecf6474881af2fd8e208bbd9898cb
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-04-11 07:24:59 +0900

    Change AssemblyFileVersion

--- Overlayer.Bootstrapper/Properties/AssemblyInfo.cs
+++ Overlayer.Bootstrapper/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("a10da92a-c5bb-4708-b1b4-9a112a644d8c")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.48.0.0")]
 
 [assembly: NeutralResourcesLanguage("")]
